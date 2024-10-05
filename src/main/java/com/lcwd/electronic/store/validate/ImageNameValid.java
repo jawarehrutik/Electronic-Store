6fcc248236d5f95f0123834a0b51de47a63b0926
jawarehrutik <jawarehrutik1999@gmail.com>
2024-10-05 13:41:36 +0530

    Second Commit

---
+++ src/main/java/com/lcwd/electronic/store/validate/ImageNameValid.java
@@ -0,0 +1,13 @@
+package com.lcwd.electronic.store.validate;
+
+import org.hibernate.annotations.Tables;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+
+public interface ImageNameValid {
+
+
+}
